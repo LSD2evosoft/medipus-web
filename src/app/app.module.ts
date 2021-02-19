418a24cc4f46bcad415b5798e7d499d23daf78b9
Soma Zsják <sanraith@users.noreply.github.com>
2021-02-19 21:13:19 +0100

    Added bootstrap and ngx-bootstrap. Added example card and dropdown styles.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,29 @@ import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
 import { ToplistComponent } from './toplist/toplist.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    ProfileComponent,
-    ToplistComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        ProfileComponent,
+        ToplistComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+
+        // ngx-bootstrap https://valor-software.com/ngx-bootstrap/#/documentation#getting-started
+        BrowserAnimationsModule,
+        BsDropdownModule.forRoot(),
+        TooltipModule.forRoot(),
+        ModalModule.forRoot()
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
