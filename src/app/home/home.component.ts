872611a7f3496e2f68cb7a1d275e654e7ec0936b
LillaJuhasz <lillaajuhasz@gmail.com>
2021-02-20 21:27:26 +0100

    added modal content to checkin achievement, and fixed the progress bar on profile

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,33 +7,46 @@ import { formatDistance } from 'date-fns';
 import { ProgressbarModule } from 'ngx-bootstrap/progressbar';
 
 @Component({
-    selector: 'app-home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.scss']
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
-    constructor(public datastore: DatastoreService, private modalService: BsModalService) { }
+  isGlobal: boolean = true;
 
-    helpers = {formatDistance}
-    modalRef: BsModalRef;
-    currentScreenings: SuggestedScreening[];
-    nextScreenings: SuggestedScreening[];
-    nextScreenings2: SuggestedScreening[];
-    expiredScreenings: SuggestedScreening[];
-    modalScreening: Screening;
-    now: Date;
-   
-    openModal(screening: Screening, template: TemplateRef<any>) {
-      this.modalRef = this.modalService.show(template);
-      this.modalScreening = screening;
+  constructor(public datastore: DatastoreService, private modalService: BsModalService) { }
+
+  helpers = { formatDistance }
+  modalRef: BsModalRef;
+  currentScreenings: SuggestedScreening[];
+  nextScreenings: SuggestedScreening[];
+  nextScreenings2: SuggestedScreening[];
+  expiredScreenings: SuggestedScreening[];
+  modalScreening: Screening;
+  now: Date;
+
+  openModal(screening: Screening, template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+    this.modalScreening = screening;
+  }
+
+  modalSuccessFunction() {
+    if (this.isGlobal) {
+      this.isGlobal = false;
     }
-    
-    ngOnInit(): void {
-      this.now = new Date();
-      this.expiredScreenings = this.datastore.user.suggestedScreenings.filter(item => item.date < this.now) ?? null;
-      this.nextScreenings2 = this.datastore.user.suggestedScreenings.filter(item => item.date >= this.now);
-      this.currentScreenings = [this.datastore.user.suggestedScreenings[0]];
-      this.nextScreenings = [this.datastore.user.suggestedScreenings[1]];
+    else {
+      setTimeout(() => {
+        this.isGlobal = true;
+      }, 1000);
     }
+  }
+
+  ngOnInit(): void {
+    this.now = new Date();
+    this.expiredScreenings = this.datastore.user.suggestedScreenings.filter(item => item.date < this.now) ?? null;
+    this.nextScreenings2 = this.datastore.user.suggestedScreenings.filter(item => item.date >= this.now);
+    this.currentScreenings = [this.datastore.user.suggestedScreenings[0]];
+    this.nextScreenings = [this.datastore.user.suggestedScreenings[1]];
+  }
 }
